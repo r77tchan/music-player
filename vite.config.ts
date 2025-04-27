da23a1c13ceeaad70739d6216f543b532fc0d086
r77tchan <r77tchan@gmail.com>
2025-04-27 13:58:03 +0900

    🐶

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/music-player',
+  base: '/music-player/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
